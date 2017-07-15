dcca78a0eacadbb26090dde3839f7d49b4e579a5
Your Name <you@example.com>
2017-07-15 13:08:45 +0530

    cec

--- index.js
+++ index.js
@@ -1,42 +1,28 @@
-var app = require('express')();
-var http = require('http').Server(app);
+var express = require('express');
+var http = require('http');
 var io = require('socket.io')(http);
-  var config = require('cloud-env')
+var config = require('cloud-env')
+
+
 
 
 var server_port = config.PORT || 8080;
 var server_ip_address = config.IP || '127.0.0.1';
 
+var app = express();
+
+
+console.log('Server up and running @ '+server_ip_address+':'+server_port);
 
 app.get('/',function(req,res)
 {
-  res.sendFile(__dirname + '/htmls/connect.html');
-});
 
-io.on("connection",function(socket)
-{
+console.log('Request Recieved');
 
-socket.on('Data',function(msg){
-
-console.log(msg);
-
-io.emit('retrn',msg);
-
-
-
-});
-console.log('User Connected');
-
-socket.on('disconnect',function(){
-console.log('User Disconnected');
-});
+   res.sendFile(__dirname + '/htmls/connect.html');
 
 });
 
 
+var server = app.listen(server_port,function(){});
 
-
-http.listen(server_port,server_ip_address,function(){
-console.log('listening on! ');
-
-});

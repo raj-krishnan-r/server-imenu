9a119c495f373b5d3ff1aa665c480487a066c1da
Your Name <you@example.com>
2017-07-15 10:34:40 +0530

    Init

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+
 app.get('/',function(req,res)
 {
   res.sendFile(__dirname + '/htmls/connect.html');
@@ -30,7 +34,7 @@ console.log('User Disconnected');
 
 
 
-http.listen(3000,function(){
+http.listen(server_port,server_ip_address,function(){
 console.log('listening on! ');
 
 });

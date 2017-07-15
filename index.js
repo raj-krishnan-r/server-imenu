699a4773a97b6bae22a4d57c31e8a0b9372768b3
Your Name <you@example.com>
2017-07-15 14:02:56 +0530

    lol

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ var io = require('socket.io')(http);
 var config = require('cloud-env')
 
 
+
+
+var server_port = config.PORT || 8080;
+var server_ip_address = config.IP || '127.0.0.1';
+
+
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080  
 , ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
@@ -22,17 +28,5 @@ console.log('Request Recieved');
 });
 
 
-
-io.on('connection', function(socket){
-  
-socket.on('Data',function(msg)
-{
-io.emit('retrn',msg);
-
-});
-
-console.log('a user connected');
-});
-
 var server = app.listen(port,ip,function(){});
-io.listen(server);
+

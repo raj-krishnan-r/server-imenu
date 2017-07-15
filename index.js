1f4d0f9e55c6e0052b3a4d4f330411bef80cbd98
Your Name <you@example.com>
2017-07-15 15:53:27 +0530

    newhome

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var app = require('http').createServer(handler)
+var app = require('http').createServer(handler);
 var io = require('socket.io')(app);
 var fs = require('fs');
 
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
 var ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.listen(port,ip);
-
 function handler (req, res) {
 
   fs.readFile(__dirname + '/htmls/connect.html',
